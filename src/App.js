e05e9bcc4b3e8ceb6e230c545a581e9bb141542a
zaycation.dev <isaiahthomas098@gmail.com>
2020-12-15 17:07:54 -0800

    css grid layout and nav setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { MdAccountCircle } from "react-icons/md";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <nav className="nav__content">
+        <h4>TECH⋄SHIP</h4>
+        <a href="/">MISSION</a>
+        <a href="/">PROGRAMS</a>
+        <a href="/">DONATE</a>
+        <MdAccountCircle size={38} color="white" />
+      </nav>
+      <main className="main__content">Main</main>
+      <div className="content1__content">Content1</div>
+      <div className="content2__content">Content2</div>
+      <div className="content3__content">Content3</div>
+      <footer className="footer__content">Footer</footer>
     </div>
   );
 }
