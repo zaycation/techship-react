56d854e4216b662ad3af4dbdf80e4ec7bb5dfd61
zaycation.dev <isaiahthomas098@gmail.com>
2020-12-16 16:39:21 -0800

    added routing and external pricing page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import "./App.css";
 import { MdAccountCircle } from "react-icons/md";
+import { Button } from "react-bootstrap";
 
 function App() {
   return (
     <div id="container">
       <nav className="nav__content">
-        <h4>TECH⋄SHIP</h4>
+        <a href="/">
+          <h4>TECH~SHIP</h4>
+        </a>
         <a href="/">MISSION</a>
         <a href="/">PROGRAMS</a>
         <a href="/">DONATE</a>
         <MdAccountCircle size={38} color="white" />
       </nav>
-      <main className="main__content">Main</main>
+      <main className="main__content">
+        <h2>TECH~SHIP</h2>
+        <h4 className={"text-center"}>
+          Silicon Valley Based Non-Profit Dedicated to Mentoring and Educating
+          Our Youth
+        </h4>
+        <Button variant="outline-light">Apply With us Today</Button>
+      </main>
       <div className="content1__content">Content1</div>
       <div className="content2__content">Content2</div>
       <div className="content3__content">Content3</div>
